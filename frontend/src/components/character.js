5f34a99545a90804f10983edea2d4ecc0f281eba
Horace Velmont <dragond7@naver.com>
2019-08-28 21:04:26 +0900

    캐릭터 띄우기

--- frontend/src/components/character.js
+++ frontend/src/components/character.js
@@ -1,32 +1,34 @@
 import React, {Component} from 'react';
 import girl_main from '../assets/character/girl_main.png';
 import {observer} from 'mobx-react';
+import Matter from "matter-js";
 
-import {Body, Sprite} from 'react-game-kit'
+import {Sprite} from 'react-game-kit'
 import {PropTypes} from "prop-types";
 
 export default @observer
 
 class Character extends Component {
   static propTypes = {
-    keys: PropTypes.object,
-    onEnterBuilding: PropTypes.func,
-    store: PropTypes.object,
+    x: PropTypes.number,
+    y: PropTypes.number,
+    onUpdate: PropTypes.func
   };
+
+  static defaultProps = {
+    x: 0,
+    y: 0
+  };
+
 
   constructor(props) {
     super(props);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
+    this.state = {
+      x: props.x,
+      y: props.y
+    };
 
-  getWrapperStyles() {
-    const {characterPosition} = this.props.store;
-    const {x, y} = characterPosition;
-    return {
-      position: 'absolute',
-      transform: `translate(${x}px, ${y}px)`,
-      transformOrigin: 'left top'
-    }
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   handleKeyDown(e) {
@@ -36,18 +38,40 @@ class Character extends Component {
   }
 
   update() {
-    const {store} = this.props;
-    const {body} = this.body;
-    body.position.x += 1;
-    store.setCharacterPosition(body.position);
+    const {onUpdate} = this.props;
+
+    if (onUpdate && typeof onUpdate === "function") {
+      const onUpdateResult = onUpdate(this.state);
+
+      if (onUpdateResult) {
+        this.setState((prevState) => {
+          return {...prevState, ...onUpdateResult};
+        });
+      }
+    }
+    if (this.body.body) {
+      Matter.Body.setVelocity(this.body.body, {x: this.state.x, y: this.state.y});
+    }
+  }
+
+  getImageStyles() {
+    const scaledWidth = Math.round(this.props.width * this.context.scale);
+
+    return {
+      width: scaledWidth,
+      imageRendering: 'pixelated'
+    };
   }
 
   render() {
     return (
-      <div style={this.getWrapperStyles()}>
-        <Body args={[0, 0, 75, 75]} ref={b => this.body = b}>
-        <Sprite src={girl_main} onKeyPress={this.handleKeyDown}/>
-        </Body>
+      <div className="character">
+
+        <img src={girl_main} style={{
+          position: "absolute",
+          left: this.state.x,
+          top: this.state.y
+        }}/>
       </div>
     )
   }
